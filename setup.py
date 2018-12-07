ea67c97e4fed92190ba5e280195c1ccae3f4ce50
andreykurenkov <andreyvkurenkov@gmail.com>
2018-12-06 17:45:29 -0800

    Got a working Gym pacman env

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pacman-env',
+      version='1.0',
+      description='Pacman Env',
+      install_requires=[
+          'gym',
+      ],
+      author='Andrey Kurenkov',
+      author_email='andreyk@stanford.edu',
+      url='https://github.com/andreykurenkov/pacman-env',
+      packages=['pacman_env'],
+     )
