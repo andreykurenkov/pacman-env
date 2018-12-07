bc1a35bf82d3c26d81b373e618391544122a3dc0
Andrey Kurenkov <andreyvkurenkov@gmail.com>
2018-12-07 07:58:42 -0800

    Various tweaks and fixes

--- setup.py
+++ setup.py
@@ -12,4 +12,6 @@ setup(name='pacman-env',
       author_email='andreyk@stanford.edu',
       url='https://github.com/andreykurenkov/pacman-env',
       packages=['pacman_env'],
+      package_dir={'': 'src'},
+      package_data={'pacman_env': ['layouts/*.lay']},
      )
